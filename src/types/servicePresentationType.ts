b6c59745e23cc0b75a3a9f99a03872a42c376997
Draxx0 <william.fort.lmgl@gmail.com>
2023-09-15 11:42:55 +0200

    :sparkles: services section done

--- src/types/servicePresentationType.ts
+++ src/types/servicePresentationType.ts
@@ -4,6 +4,8 @@ import { TestimonialCardTheme } from "../components/service/TestimonialCard";
 export interface servicePresentationType {
   services: {
     image: string;
+    card_theme: "white" | "blue";
+    display: "row" | "row-reverse";
     image_logos: {
       image: string;
     }[];
